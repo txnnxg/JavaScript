717ae619a66409598a8d7b82952d291bb2778c57
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-02-14 17:41:16 -0300

    Exercicio feito

--- cursojs/aula013/aula015/ex015/script.js
+++ cursojs/aula013/aula015/ex015/script.js
@@ -1,1 +1,22 @@
-alert('Olá Mundo')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var genero = ``
+       var img = document.createAttribute('img')
+       img.setAttribute('id', 'foto')
+       if (fsex[0].checked) {
+            genero = 'Homem'
+       } else if (fsex[1].checked) {
+            genero = 'Mulher'
+       }
+       res.style.textAlign = `center`
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
