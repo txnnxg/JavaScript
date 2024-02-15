d313d04c289676dee34e6da40962a7664bad6b7a
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-02-15 19:18:02 -0300

    criando o codigo no JS

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+var n1 = document.getElementById('n1')
+var n2 = document.getElementById('n2')
+var pas = document.getElementById('pulo')
+function clicou() {
+    if (n1 > n2) {
+
+    } if else (n2 > n1) {
+
+    } else (n1 == 0 && n2 == 0) {
+        
+    }
+}
