655062d3f1072ce7001438650eab8b5a056ac6ad
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-02-15 23:48:48 -0300

    att no JS

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,12 +1,16 @@
 var n1 = document.getElementById('n1')
 var n2 = document.getElementById('n2')
 var pas = document.getElementById('pulo')
+var msg = document.getElementById('msg')
 function clicou() {
     if (n1 > n2) {
+            for(n1 = n1 && n2 = n2; n1 <= n2; n1++ ) {
 
+            }
     } if else (n2 > n1) {
 
     } else (n1 == 0 && n2 == 0) {
         
     }
 }
+msg.innerHTML('Preparando a contagem')
