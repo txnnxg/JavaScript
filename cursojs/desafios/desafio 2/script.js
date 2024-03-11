d64f4e5becd375d05f349797b8cf89ee2d625e24
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-03-11 10:43:11 -0300

    exercicio feito

--- cursojs/desafios/desafio 2/script.js
+++ cursojs/desafios/desafio 2/script.js
@@ -3,9 +3,16 @@ function carregar() {
     let foto = window.document.getElementById('imagem')
     let data = new Date()
     let hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora}`
-    if (hora > 0 && hora <= 22) {
-            foto.src = 'foto-manha.jpg'
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+            foto.src = 'imagens/foto-manha.jpg'
+            document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+            foto.src = 'imagens/foto-tarde.jpeg'
+            document.body.style.background = '#b9846f'
 
+    } else {
+            foto.src = 'imagens/foto-noite.jpg'
+            document.body.style.background = '#515154'
     }
 }
