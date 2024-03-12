f3817146839e7aa542557f28751feba688fdc5bd
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-03-12 00:36:28 -0300

    40% do exercicio

--- cursojs/desafios/desafio 3/script.js
+++ cursojs/desafios/desafio 3/script.js
@@ -1,3 +1,8 @@
 function verificar() {
-    alert('Deu certo')
+    let ano = window.document.getElementById('txtano')
+    let S = window.document.getElementsByClassName('OPCAO')
+    let res = window.document.getElementById('res')
+    if ( S == "op1") {
+        alert('Deu certo')
+    }
 }
