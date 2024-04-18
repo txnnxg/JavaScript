9e9cb3ade0cbb8c92d1845cbecf725346e5d4c56
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-04-18 11:30:09 -0300

    Exercicio criado

---
+++ cursojs/aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+function contagem() {
+    let ini = window.document.getElementById('ini')
+    let fim = window.document.getElementById('fim')
+    let pas = window.document.getElementById('pas')
+    let res = window.document.getElementById('res')
+    for ( ini ; ini <= fim ; ini = ini + pas) {
+            window
+    }
+    
+}
