9a99042c6734959ab0de37b0c9792ce84d3bbae1
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-04-21 19:31:50 -0300

    Tabuada criada

--- cursojs/aula14ex/ex017/script.js
+++ cursojs/aula14ex/ex017/script.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function gerar() {
+    let numero = document.getElementById('numero')
+    let res = document.getElementById('res')
+    if (numero.value.length == 0) {
+        window.alert('Deu Erro')
+    } else {
+        let n = Number(numero.value)
+        res.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let r = n * c
+            res.innerHTML += `${n} * ${c} = ${r} <br>`  
+        }
+    }
+}
