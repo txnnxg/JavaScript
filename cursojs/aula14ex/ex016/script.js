3e58c8516cb24c56d43abefa495b10d4c9ad9651
txnnxg <143152390+txnnxg@users.noreply.github.com>
2024-04-21 18:57:40 -0300

    Contador criado

--- cursojs/aula14ex/ex016/script.js
+++ cursojs/aula14ex/ex016/script.js
@@ -3,8 +3,24 @@ function contagem() {
     let fim = window.document.getElementById('fim')
     let pas = window.document.getElementById('pas')
     let res = window.document.getElementById('res')
-    for ( ini ; ini <= fim ; ini = ini + pas) {
-            window
-    }
-    
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.lenght == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else if (ini.value.length > fim.value.length ) {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        for ( let c = i ; c >= f ; c -= p) {
+            res.innerHTML += `${c} `
+            }
+    } 
+        else {
+        res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(pas.value)
+    for ( let c = i ; c <= f ; c += p) {
+            res.innerHTML += `${c} `
+            }
+    } 
 }
